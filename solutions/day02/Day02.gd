39d2f2d2fb175aa23bb9a9289577b9bd6e71e8b7
Dexter Gramfors <dexter.gramfors@trioptima.com>
2021-12-02 13:21:50 -0500

    Solve day 2 part 2

--- solutions/day02/Day02.gd
+++ solutions/day02/Day02.gd
@@ -10,6 +10,19 @@ func part1(instructions):
 			["up", var n]: y -= n
 	return x * y
 
+func part2(instructions):
+	var x = 0
+	var y = 0
+	var aim = 0
+	for instruction in instructions:
+		match instruction:
+			["forward", var n]:
+				x += n
+				y += aim * n
+			["down", var n]: aim += n
+			["up", var n]: aim -= n
+	return x * y
+
 func solve():
 	var text = Utils.slurp("res://solutions/day02/input.txt")
 	var instructions = []
@@ -17,6 +30,7 @@ func solve():
 		var parts = line.split(" ")
 		instructions.push_back([parts[0], int(parts[1])])
 	print(part1(instructions))
+	print(part2(instructions))
 
 func _ready():
 	solve()
