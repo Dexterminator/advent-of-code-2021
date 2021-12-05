ca9ffddd60d23c49d1ef2cb6d783768f42f7f9f3
Dexter Gramfors <dexter.gramfors@gmail.com>
2021-12-05 13:13:09 -0500

    Solve day 5 part 2

--- solutions/day05/Day05.gd
+++ solutions/day05/Day05.gd
@@ -1,20 +1,7 @@
 extends Node2D
 
-# var input = """0,9 -> 5,9
-# 8,0 -> 0,8
-# 9,4 -> 3,4
-# 2,2 -> 2,1
-# 7,0 -> 7,4
-# 6,4 -> 2,0
-# 0,9 -> 2,9
-# 3,4 -> 1,4
-# 0,0 -> 8,8
-# 5,5 -> 8,2"""
-
 func nums_between(n1, n2):
 	var delta = n1 - n2
-	if delta == 0:
-		return null
 	var s = -sign(delta)
 	var nums = range(n1+s, n2, s)
 	nums.push_back(n1)
@@ -29,7 +16,24 @@ func get_score(count_by_coord):
 			score += 1
 	return score
 
-func part1(lines):
+func add_coord_count(count_by_coord, coord):
+	count_by_coord[coord] = count_by_coord.get(coord, 0) + 1
+
+func add_diagonal_coord_counts(count_by_coord, x1, x2, y1, y2):
+	var x_nums_between = nums_between(x1, x2)
+	var y_nums_between = nums_between(y1, y2)
+	for i in len(x_nums_between):
+		add_coord_count(count_by_coord, "%s,%s" % [x_nums_between[i], y_nums_between[i]])
+
+func add_horizontal_coord_counts(count_by_coord, x1, x2, y1, y2):
+	if x1 == x2:
+		for y in nums_between(y1, y2):
+			add_coord_count(count_by_coord, "%s,%s" % [x1, y])
+	elif y1 == y2:
+		for x in nums_between(x1, x2):
+			add_coord_count(count_by_coord, "%s,%s" % [x, y1])
+
+func part2(lines):
 	var count_by_coord = {}
 	for line in lines:
 		var x1 = int(line[0])
@@ -38,16 +42,22 @@ func part1(lines):
 		var y2 = int(line[3])
 
 		if x1 != x2 and y1 != y2:
-			continue
+			add_diagonal_coord_counts(count_by_coord, x1, x2, y1, y2)
+		else:
+			add_horizontal_coord_counts(count_by_coord, x1, x2, y1, y2)
 
-		if x1 == x2:
-			for y in nums_between(y1, y2):
-				var coord = "%s,%s" % [x1, y]
-				count_by_coord[coord] = count_by_coord.get(coord, 0) + 1
-		elif y1 == y2:
-			for x in nums_between(x1, x2):
-				var coord = "%s,%s" % [x, y1]
-				count_by_coord[coord] = count_by_coord.get(coord, 0) + 1
+	return get_score(count_by_coord)
+
+func part1(lines):
+	var count_by_coord = {}
+	for line in lines:
+		var x1 = int(line[0])
+		var y1 = int(line[1])
+		var x2 = int(line[2])
+		var y2 = int(line[3])
+
+		if x1 == x2 or y1 == y2:
+			add_horizontal_coord_counts(count_by_coord, x1, x2, y1, y2)
 
 	return get_score(count_by_coord)
 
@@ -55,6 +65,7 @@ func solve():
 	var input = Utils.slurp("res://solutions/day05/input.txt")
 	var lines = Utils.regex_group_list(input, "(\\d+),(\\d+) -> (\\d+),(\\d+)")
 	print(part1(lines))
+	print(part2(lines))
 
 func _ready():
 	solve()
